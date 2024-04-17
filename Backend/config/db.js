53a51b2e37de5743d34a0fe0068363033db95961
kundan761 <kundankumarsuraj761@gmail.com>
2024-04-18 02:51:08 +0530

    env file setup

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv')
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host:"localhost",
-  user: "root",
-  password:"root",
+  host:process.env.host,
+  user: process.env.user,
+  password:process.env.password,
   database:"jokes"
 });
 
